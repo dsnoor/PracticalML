759c7938025765949d649d90e69c0ba1442dc61a
Noor Ahmed email = alacrity.n@gmail.com <alacrity.n@gmail.com>
2015-01-26 00:54:14 +0530

    add files to repo

---
+++ data.R
@@ -0,0 +1,32 @@
+# Title:  data.R
+# Author: Noor Ahmed
+# Date:   2015-Jan-26
+
+
+# Grab from the Internet --------------------------------------------------
+download.file ("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
+               destfile = "train.csv")
+download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
+              destfile = "test.csv")
+
+# Read CSV's --------------------------------------------------------------
+train <- read.csv("train.csv")
+test  <- read.csv("test.csv")
+
+# Clean Up the Data -------------------------------------------------------
+test$X       <- NULL
+train$X      <- NULL
+train.clean  <- train[,colSums(is.na(train)) < .5 * nrow(train)]
+test.clean   <- test[,colSums(is.na(test)) < .5 * nrow(test)]
+train.clean0 <- train.clean[,colnames(train.clean) %in% colnames(test.clean)]
+
+train.clean0$classe <- train.clean$classe
+
+# Partition Labeled Data into Training and Testing ------------------------
+# 60-40 Split
+inTrain       <- createDataPartition(train.clean0$classe, p=0.6, list=FALSE)
+labeled.train <- train.clean0[inTrain,]
+labeled.test  <- train.clean0[-inTrain,]
+
+# Save Image --------------------------------------------------------------
+save.image("ml_project_data.Rdata")
---
+++ functions.R
@@ -0,0 +1,32 @@
+# Title:  functions.R
+# Author: Noor Ahmed
+# Date:   2015-Jan-26
+
+
+# Load Libraries ----------------------------------------------------------
+require(caret)
+require(data.table)
+require(MASS)
+require(mlogit)
+require(grid)
+require(gtable)
+require(rattle)
+require(randomForest)
+require(rpart)
+require(sqldf)
+require(tm)
+
+# Custom Functions --------------------------------------------------------
+ml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+
+xyform <- function (y_var, x_vars) {
+  # y_var: a length-one character vector
+  # x_vars: a character vector of object names
+  as.formula(sprintf("%s ~ %s", y_var, paste(x_vars, collapse = " + ")))
+}
